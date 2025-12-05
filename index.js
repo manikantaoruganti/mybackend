d89801edfd94cc7500e66c3d14383401e3c64b63
Manikanta <orugantimanikanta24@gmail.com>
2025-12-05 11:19:04 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// import http from 'http';
+
+// const server = http.createServer((req, res) => {
+//     console.log(req.method);
+//     if(req.url=="/users"){
+//          res.writeHead(200, { "content-type": "application/json" });
+//          res.end(JSON.stringify({name: "thub"}))
+//     }else
+//     res.writeHead(200, { "content-type": "text/plain" });
+//     res.end("hello this is from backend");
+// });
+// server.listen(7007, () => {
+//     console.log(`server running at port ${7007}`);
+// });
+
+
