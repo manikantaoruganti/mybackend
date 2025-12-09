088193a9dba96d712dedc99f4a50d83ddce91388
Manikanta <orugantimanikanta24@gmail.com>
2025-12-09 10:35:44 +0530

    added methods

--- index.js
+++ index.js
@@ -45,26 +45,68 @@
 // server.listen(7007, () => {
 //     console.log(`server running at port ${7007}`);
 // });
-  import express from 'express';
+//   import express from 'express';
 
-const app = express();
+// const app = express();
 
 
+// app.use(express.json());
+
+// app.post('/add-user', (req, res) => {
+//   let mydata = req.body;
+//   console.log(mydata);
+//   res.send("data added");
+// });
+
+// app.get('/users', (req, res) => {
+//   console.log("hello this is");
+//   res.send("all users");
+// });
+// app.listen(7007, () => {
+//   console.log(`server running at port 7007`);
+// });
+
+import express from 'express';
+const  app = express();
 app.use(express.json());
 
 app.post('/add-user', (req, res) => {
   let mydata = req.body;
-  console.log(mydata);
+  console.log("POST /add-user:", mydata);
   res.send("data added");
 });
-
 app.get('/users', (req, res) => {
-  console.log("hello this is");
+  console.log("GET /users");
   res.send("all users");
+});
+app.get('/users/:id', (req, res) => {
+  let id = req.params.id;
+  console.log("GET /users/" + id);
+  res.send("single user " + id);
+});
+ 
+app.put('/users/:id', (req, res) => {
+  let id = req.params.id;
+  let data = req.body;
+  console.log("PUT /users/" + id, data);
+  res.send("user " + id + " updated");
+});
+ 
+app.patch('/users/:id', (req, res) => {
+  let id = req.params.id;
+  let data = req.body;
+  console.log("PATCH /users/" + id, data);
+  res.send("user " + id + " partially updated");
+});
+
+
+app.delete('/users/:id', (req, res) => {
+  let id = req.params.id;
+  console.log("DELETE /users/" + id);
+  res.send("user " + id + " deleted");
 });
 app.listen(7007, () => {
   console.log(`server running at port 7007`);
 });
-
 
  
